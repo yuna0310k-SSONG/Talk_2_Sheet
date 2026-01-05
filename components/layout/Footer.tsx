0c39b94c1bfb937701805fd930093502af4a8c37
Yuna <una3917@naver.com>
2026-01-05 11:35:21 +0900

    20260105

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="border-t bg-white py-8">
       <div className="container mx-auto px-4 text-center text-sm text-gray-600">
-        <p>© 2025 Talk 2 Sheet. All rights reserved.</p>
+        <p>© 2026 Talk 2 Sheet. All rights reserved.</p>
         <p className="mt-2">
           Made by{" "}
           <a
